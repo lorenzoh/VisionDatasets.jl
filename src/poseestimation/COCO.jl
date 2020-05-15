dcba340983d3abec4f28964fffd2108730b3e48b
lorenzoh <lorenz.ohly@gmail.com>
2020-05-15 11:22:39 +0200

    Rewrote COCO data loading

--- src/poseestimation/COCO.jl
+++ src/poseestimation/COCO.jl
@@ -2,6 +2,9 @@ module COCO
 
 using DataDeps
 using JSON3
+using JuliaDB
+using Images
+using LearnBase
 using PoseEstimation: Joint, PoseConfig
 using StaticArrays
 
@@ -27,18 +30,13 @@ THIS SOFTWARE AND ANNOTATIONS ARE PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBU
 
 
 function __init__()
-
-    # Register keypointannotations valid and train
     register(DataDep(
         "coco_keypoint_annotations",
         COCO_COPYRIGHT,
         "http://images.cocodataset.org/annotations/annotations_trainval2017.zip",
         "113a836d90195ee1f884e704da6304dfaaecff1f023f49b6ca93c4aaae470268",
-        post_fetch_method = unpack
+        post_fetch_method = path -> (unpack(path); preparecoco(dirname(path)))
     ))
-
-
-
 end
 
 
@@ -51,101 +49,112 @@ const CONFIG = PoseConfig(
         "left_knee","right_knee","left_ankle","right_ankle"
     ],
     [
-        (16,14),
-        (14,12),
-        (17,15),
-        (15,13),
-        (12,13),
-        (6,12),
-        (7,13),
-        (6,7),
-        (6,8),
-        (7,9),
-        (8,10),
-        (9,11),
-        (2,3),
-        (1,2),
-        (1,3),
-        (2,4),
-        (3,5),
-        (4,6),
-        (5,7)
+        (16, 14),
+        (14, 12),
+        (17, 15),
+        (15, 13),
+        (12, 13),
+        (6, 12),
+        (7, 13),
+        (6, 7),
+        (6, 8),
+        (7, 9),
+        (8, 10),
+        (9, 11),
+        (2, 3),
+        (1, 2),
+        (1, 3),
+        (2, 4),
+        (3, 5),
+        (4, 6),
+        (5, 7)
     ]
 )
 
-function coco_keypoints(imagespath, annotationpath = datadep"coco_keypoint_annotations")
-    trainannspath = joinpath(annotationpath, "annotations/person_keypoints_train2017.json")
-    validannspath = joinpath(annotationpath, "annotations/person_keypoints_val2017.json")
+const MEANS = SVector(0.46339586534149074, 0.44807951561830006, 0.41191946181562883)
+const STDS = SVector(0.2353513819415526, 0.2328964398675012, 0.23061464879094382)
 
-    trainanns = JSON3.read(read(trainannspath))
-    validanns = JSON3.read(read(validannspath))
 
-    imagefiledict = merge(
-        Dict(image.id => "train2017/$(image.file_name)" for image in trainanns.images),
-        Dict(image.id => "val2017/$(image.file_name)" for image in validanns.images)
-    )
+struct COCOKeypoints
+    imagefolder
+    t::IndexedTable
+    COCOKeypoints(imagefolder) = new(
+        imagefolder,
+        JuliaDB.load(joinpath(datadep"coco_keypoint_annotations", "coco.jdb")))
+end
 
-    traindict = groupby(trainanns.annotations, :image_id)
-    validdict = groupby(validanns.annotations, :image_id)
 
-    splitdict = Dict()
-    for (d, split) in zip((traindict, validdict), (:train, :valid))
-        for key in keys(d)
-            splitdict[key] = split
+function LearnBase.getobs(ds::COCOKeypoints, idx)
+    annotation = ds.t[idx]
+    image = Images.load(
+        getimagefile(ds.imagefolder, annotation.image_id, annotation.isvalid))
+
+    return (image = image, pose = annotation.keypoints, config = CONFIG)
+end
+
+LearnBase.nobs(ds::COCOKeypoints) = length(ds.t)
+
+
+getimagefile(folder, image_id, isvalid) = joinpath(
+    folder,
+    isvalid ? "val2017" : "train2017",
+    "$(lpad(image_id, 12, "0")).jpg"
+)
+
+# Dataset preparation
+
+function preparecoco(annotationsfolder)
+    trainannspath = joinpath(annotationsfolder, "annotations/person_keypoints_train2017.json")
+    validannspath = joinpath(annotationsfolder, "annotations/person_keypoints_val2017.json")
+
+    trainannot = JSON3.read(read(trainannspath))
+    validannot = JSON3.read(read(validannspath))
+
+    t = makecocotable(trainannot.annotations, validannot.annotations)
+    save(t, joinpath(annotationsfolder, "coco.jdb"))
+    return t
+end
+
+
+function makecocotable(trainanns, validanns)
+    traint = annotationtable(trainanns, isvalid = false)
+    validt = annotationtable(validanns, isvalid = true)
+    return merge(traint, validt)
+end
+
+function annotationtable(annotations; isvalid = false)
+    columns = [:num_keypoints, :area, :keypoints, :bbox, :id, :image_id]
+    data = Dict(s => [ann[s] for ann in annotations] for s in columns)
+    data[:num_keypoints] = UInt8.(data[:num_keypoints])
+    data[:area] = Float32.(data[:area])
+    data[:bbox] = parsebbox.(data[:bbox])
+    data[:keypoints] = parsekeypoints.(data[:keypoints])
+    data[:isvalid] = fill(isvalid, length(data[:num_keypoints]))
+
+    t = table(data, pkey = :image_id)
+    return t
+end
+
+
+function parsebbox(bbox)
+    y, x, h, w = bbox
+    return [SVector{2,Float32}(y + 1, x + 1), SVector{2,Float32}(y + h, x + w)]
+end
+
+
+function parsekeypoints(keypoints)
+    xs = @view keypoints[1:3:end]
+    ys = @view keypoints[2:3:end]
+    ids = @view keypoints[3:3:end]
+    pose = Vector{Union{Nothing, SVector{2, Float32}}}(nothing, length(xs))
+
+    for (k, (x, y, id)) in enumerate(zip(xs, ys, ids))
+        if id != 0
+            pose[k] = SVector{2,Float32}(y + 1, x + 1)
         end
     end
 
-    anns = merge(traindict, validdict)
-
-    return PoseDataset(
-        [imagefiledict[image_id] for (image_id, _) in anns],
-        [parseannposes(ann) for (_, ann) in anns],
-        CONFIG,
-        imagespath,
-        Dict(
-            :missingbboxes => [parsemissingboxes(ann) for (_, ann) in anns]
-        ),
-        Dict(
-            :split => [splitdict[path] for (path, _) in anns],
-            :stats => Dict(
-                :means => [0.46339586534149074, 0.44807951561830006, 0.41191946181562883],
-                :stds => [0.2353513819415526, 0.2328964398675012, 0.23061464879094382],
-            ),
-        )
-    )
-end
-
-
-function parseannposes(ann)
-    kps::Vector{Vector{Int}} = collect(map(a -> collect(a.keypoints), filter(a -> a.num_keypoints > 0, ann)))
-    #kps = [a.keypoints for a in ann if a.num_keypoints > 0]
-    length(kps) > 0 || return []
-    nposes::Int = length(kps)
-    nkeypoints::Int = length(kps[1]) ÷ 3
-    poses = Array{Union{Nothing, SVector{2}}}(nothing, nposes, nkeypoints)
-
-    for (p, keypoints::Vector{Int}) in enumerate(kps)
-        xs = @view keypoints[1:3:end]
-        ys = @view keypoints[2:3:end]
-        ids = @view keypoints[3:3:end]
-        for (k, (x, y, id)) in enumerate(zip(xs, ys, ids))
-            if id != 0
-                poses[p, k] = SVector{2, Float32}(y + 1, x + 1)
-            end
-        end
-    end
-    return poses
-end
-
-
-function parsemissingboxes(ann)
-    return boxes = vcat([parsebbox(a.bbox) for a in ann if a.num_keypoints == 0]...)
-end
-
-
-function parsebbox(cocobbox)
-    x, y, w, h = cocobbox
-    return reshape([SVector{2, Float32}(y+1, x+1), SVector{2, Float32}(y + h, x + w)], 1, :)
+    return pose
 end
 
 end # module
